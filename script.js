a461b4595cb88907bd823e907d29917153746a5d
Fevaeiou <tintinsoliva1995@gmail.com>
2025-03-20 17:06:43 +0800

    Portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+function nextPage() {
+    window.location.href = 'yes.html';
+}
+
+function nextPagee() {
+    window.location.href = 'no.html';
+}
+
+function goBack() {
+    window.location.href = 'index.html';
+}
+
+function closePage() {
+    alert("Browser restrictions prevent closing this tab. Please close it manually.");
+}
